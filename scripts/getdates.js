12c7350634497fee465222888917f401718f7371
Adam Lind <adamsplind@gmail.com>
2025-01-08 19:47:15 -0700

    updated script

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,5 +3,10 @@ const footerEditedTime = document.getElementById("lastModified");
 const today = new Date();
 const modifiedDate = new Date(document.lastModified);
 
+const hours = today.getHours().toString().padStart(2, '0');
+const minutes = today.getMinutes().toString().padStart(2, '0');
+const seconds = today.getSeconds().toString().padStart(2, '0');
+
+
 footerYear.innerHTML = `${today.getFullYear()}`;
-footerEditedTime.innerHTML = `Last edit: ${modifiedDate}`;
+footerEditedTime.innerHTML = `Last edit: ${modifiedDate.toLocaleDateString()} ${hours}:${minutes}:${seconds}`;
