aa160e934a67f578ae264ed4db31cce9718810cd
Adam Lind <adamsplind@gmail.com>
2025-01-22 13:27:51 -0700

    minor changes

--- scripts/windChill.js
+++ scripts/windChill.js
@@ -3,17 +3,8 @@ const windChill = document.getElementById("wind-chill")
 const t = -11
 const wS = 15
 
-const calculateWindChill = (temp, windSpeed) => {
-    if (temp <= 50 && windSpeed > 3) {
-        return 35.74 + 0.6215*temp - 35.75*(windSpeed**0.16) + 0.4275*temp*(windSpeed**0.16)
-    }
-    else
-        return "N/A"
+const calculateWindChill = (temp, windSpeed) => 35.74 + 0.6215*temp - 35.75*(windSpeed**0.16) + 0.4275*temp*(windSpeed**0.16)
 
+if (t <= 50 && wS > 5) {
+    windChill.innerHTML = `${parseFloat(calculateWindChill(t, wS)).toFixed(2)} ºF`
 }
-
-const trimmedChill = (t, wS) => {
-    return parseFloat(calculateWindChill(t, wS)).toFixed(2);
-}
-
-windChill.innerHTML = `${trimmedChill(t, wS)} ºF`
